92418a3ff1b79de350ddc1338b0daa891787244f
Gurraiah123 <142591366+Gurraiah123@users.noreply.github.com>
2026-05-04 12:14:35 +0530

    Update AuthController.java

--- src/main/java/com/devops/auth/AuthController.java
+++ src/main/java/com/devops/auth/AuthController.java
@@ -1,3 +1,11 @@
+package com.devops.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import com.devops.auth.entity.User;
+import com.devops.auth.service.AuthService;
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -6,8 +14,7 @@ public class AuthController {
     private final AuthService service;
 
     @PostMapping("/register")
-    public String register(
-            @RequestBody User user){
+    public String register(@RequestBody User user) {
 
         service.register(user);
 
@@ -15,15 +22,8 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public String login(
-            @RequestBody User user){
+    public String login(@RequestBody User user) {
 
         return service.login(user);
     }
 }
-package com.devops.auth.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import com.devops.auth.service.AuthService;
-import com.devops.auth.entity.User;
